c6ea1d60ed8325279e13f975abb19e8eea030c8c
Paul Grenwood <hello@thegrenwoods.com>
2020-02-20 19:46:54 -0800

    Increased Priority of Vendor Scripts
    Now Vendor Scripts Load before Beaver Builder Global Scripts

--- includes/inc.scripts.php
+++ includes/inc.scripts.php
@@ -7,4 +7,4 @@ function b42s__vendor_scripts(){
 	wp_enqueue_script( 'basicScroll', FL_CHILD_THEME_URL . '/vendor/node_modules/basicscroll/dist/basicScroll.min.js', array(), '1.0.0', true);
 	wp_enqueue_script( 'stickybits', FL_CHILD_THEME_URL . '/vendor/node_modules/stickybits/dist/stickybits.min.js', array(), '3.7.3', true);
 }
-add_action( 'wp_enqueue_scripts', 'b42s__vendor_scripts');
+add_action( 'wp_enqueue_scripts', 'b42s__vendor_scripts', 9);
